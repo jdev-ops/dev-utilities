c566fa12942ac1c2d53bc4bdbf7655c522462698
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2023-11-10 11:27:52 +0100

    update

--- lib/git_policy/prepare_commit_msg.py
+++ lib/git_policy/prepare_commit_msg.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import os
 from git import Repo
 from slugify import slugify
 
@@ -11,8 +12,11 @@ if __name__ == "__main__":
     else:
         repo = Repo(".")
         branch_name = str(repo.active_branch)
-        template = open(f".git/devops/.{slugify(branch_name)}").read()
-        template = open(sys.argv[-1], "w").write(template)
+        if os.path.exists(f".git/devops/.{slugify(branch_name)}"):
+            template = open(f".git/devops/.{slugify(branch_name)}").read()
+        else:
+            template = f""
+        open(sys.argv[-1], "w").write(template)
         sys.exit(0)
 
 
